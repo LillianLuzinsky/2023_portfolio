6f97e65a27de80b72e0f0576d95ec5df310c3192
LillianLuzinsky <eyeris@tpg.com.au>
2022-11-02 16:25:16 +1100

    added active links on navbar

---
+++ js/script.js
@@ -0,0 +1,14 @@
+ //Make navbar links active
+ const links = document.querySelectorAll(".link");
+
+ links.forEach(clickedLink => {
+     //Add onClick event
+     clickedLink.addEventListener('click', () => {
+         //Remove the active class from all links
+         links.forEach(link => {
+             link.classList.remove("active");
+         })
+         //Add the active class on the clicked tab
+         clickedLink.classList.add('active');
+     })
+ })
