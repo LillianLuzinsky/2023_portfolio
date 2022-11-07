49cfa3fe46c275e84d497add9a05141c7a0061f0
LillianLuzinsky <eyeris@tpg.com.au>
2022-11-07 12:58:12 +1100

    added progess bar at navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+//---- navbar scroll----//
+
+const scroll = new SmoothScroll('nav a[href*="#"]', {
+  speed: 800
+});
+
+  window.onscroll = function() {
+    progressBar();
+  };
+  
+  function progressBar() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
+
+//---- nav-links activate on scroll ----//  
+
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('nav .nav-container ul li');
 
@@ -18,3 +39,4 @@ window.addEventListener('scroll', ()=> {
     }
   })
 })
+
