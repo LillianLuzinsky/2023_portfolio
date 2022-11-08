ea880dd36d8bfb08ceeb12d9e071c64619658596
LillianLuzinsky <eyeris@tpg.com.au>
2022-11-08 11:16:44 +1100

    trying to fix homepage fade scroll effect

--- js/script.js
+++ js/script.js
@@ -24,9 +24,6 @@ window.addEventListener('scroll', ()=> {
 //fade home on scroll //
 const headerHome = document.getElementById('home');
     window.addEventListener('scroll', function(){
-      headerHome.style.opacity = 1 -+window.pageYOffset/750+'';
-      headerHome.style.backgroundPositionY =+ window.pageYOffset/2+'px';
+      headerHome.style.opacity = 1 -+ window.pageYOffset/1000+'';
+      headerHome.style.backgroundPositionY =+ window.pageYOffset+'px';
     })
-
-
-    // position: fixed;
